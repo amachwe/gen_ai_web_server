2097db100a65aef5819bd16441e44f34b603116d
Azahar Machwe <amachwe@gmail.com>
2024-07-05 22:46:21 +0100

    Added client

--- sample_phi3_client.py
+++ sample_phi3_client.py
@@ -1,22 +1,30 @@
 import requests
 import json
 
-## Sample Client used with wrapped Phi3 Model from Microsoft
+## Sample Client used with wrapped LLM
 if __name__ == "__main__":
-    TARGET_URL = "http://localhost:5000"
-    REQUEST_URL = f"{TARGET_URL}/request"
+
+    ## Basic setup for the client - server communication
+    BASE_URL = "http://localhost:5000"
+    REQUEST_URL = f"{BASE_URL}/request"
     headers = {
         "Content-Type": "application/json",
         "Connection": "keep-alive",
 
     }
 
-    
+    # Sample prompt for the LLM    
     prompt_math = "What is 14+5?"
+
+    # Data to be sent to the server
     data = {
         "prompt": [{"role":"user", "content":prompt_math}],
         "process_logits": False
     }
+
     print("Sent")
+    
+    # Look Ma no Transformers!
     response = requests.post(REQUEST_URL, data=json.dumps(data), headers=headers).json()
+
     print(response.get("response"))

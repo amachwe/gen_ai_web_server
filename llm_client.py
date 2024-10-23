2c00e31bce02bc81f13c9afef80775c9a4b4da1f
Azahar Machwe <amachwe@gmail.com>
2024-10-23 22:29:20 +0100

    client update

--- llm_client.py
+++ llm_client.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-   
+
 class Client(object):
     def __init__(self, target_url:str="http://localhost:5000"):
         self.request_url = f"{target_url}/request"
@@ -12,7 +12,7 @@ class Client(object):
 
         }
 
-    def send_request(self, prompt:str):
+    def send_request(self, prompt:list[dict]):
         """
         Helper method to send a request to the LLM
         """
@@ -20,14 +20,14 @@ class Client(object):
     "debug_mode": False}}), headers=self.headers).json()
         return response
     
-    def send_request(self, prompt:str, process_logits:bool=False, run_config:dict={}):
+    def send_request(self, prompt:list[dict], process_logits:bool=False, run_config:dict={}):
         """
         Helper method to send a request to the LLM
         """
         response = requests.post(self.request_url, data=json.dumps({"prompt":prompt, "process_logits":process_logits, "run_config": run_config}), headers=self.headers).json()
         return response
     
-    def send_request_with_debug(self, prompt:str, process_logits:bool=False):
+    def send_request_with_debug(self, prompt:list[dict], process_logits:bool=False):
         """
         Helper method to send a request to the LLM
         """
@@ -35,7 +35,7 @@ class Client(object):
     "debug_mode": True}}), headers=self.headers).json()
         return response
     
-    def send_request_with_sampling(self, prompt:str, process_logits:bool=False):
+    def send_request_with_sampling(self, prompt:list[dict], process_logits:bool=False):
         """
         Helper method to send a request to the LLM
         """
@@ -43,10 +43,64 @@ class Client(object):
     "debug_mode": False}}), headers=self.headers).json()
         return response
     
-    def send_request_with_sampling_and_debug(self, prompt:str, process_logits:bool=False):
+    def send_request_with_sampling_and_debug(self, prompt:list[dict], process_logits:bool=False):
         """
         Helper method to send a request to the LLM
         """
         response = requests.post(self.request_url, data=json.dumps({"prompt":prompt, "process_logits":process_logits, "run_config": {"do_sample": True,
     "debug_mode": True}}), headers=self.headers).json()
         return response
+    
+    def extract_response(self, response):
+        """
+        Helper method to extract the response from the LLM
+        """
+        return response["response"][1]["content"]
+    
+import openai
+
+class OpenAIClient(object):
+
+    def __init__(self):
+        self.client = openai.OpenAI()
+           
+
+    def send_request(self, prompt:list[dict], process_logits:bool=False, run_config:dict={}):
+        """
+        Helper method to send a request to the OpenAI LLM
+        """
+        model_name = run_config.get("model_name", "gpt-4o-mini")
+        return self.client.chat.completions.create(model=model_name, messages=prompt, logprobs=process_logits).choices[0].message
+    
+    def extract_response(self, response):
+        """
+        Helper method to extract the response from the LLM
+        """
+        return response.content
+    
+import google.generativeai as genai
+import os
+
+
+class GeminiClient(object):
+
+    def __init__(self):
+        genai.configure(api_key=os.environ["API_KEY"])
+        
+
+           
+
+    def send_request(self, prompt:list[dict], process_logits:bool=False, run_config:dict={}):
+        """
+        Helper method to send a request to the OpenAI LLM
+        """
+        model_name = run_config.get("model_name", "gemini-1.5-flash")
+        self.model = genai.GenerativeModel(model_name)
+        return self.model.generate_content(prompt[0]["content"])
+
+    
+    def extract_response(self, response):
+        """
+        Helper method to extract the response from the LLM
+        """
+        return response.text
